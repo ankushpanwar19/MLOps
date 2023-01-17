501c6809e9f361cab6074984f4f8ba04173d4e85
ankushpanwar19 <ankushpanwar19@gmail.com>
2023-01-17 20:07:28 +0100

    Custom model addition

---
+++ src/models/custom_model.py
@@ -0,0 +1,44 @@
+"""
+Script to create custome machine learning models using pytorch
+"""
+from torch import nn
+import torch.nn.functional as F
+
+
+class MnistNet(nn.Module):
+    """
+        Creating a model for MNIST image shape
+        Input Shape: 32x32x3
+        Output shape: 10
+    """
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        """
+        Forward pass for the network
+        """
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = nn.Flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+
+if __name__=="__main__":
+    model = MnistNet()
+    print(model)
