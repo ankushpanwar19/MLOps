1bf2da3c95dc43703a8d0b56163d0c93d47e40f2
ankushpanwar19 <ankushpanwar19@gmail.com>
2023-01-17 19:32:12 +0100

    Initialized the repo with cookiecutter data science structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='To create MLOps pipeline for neural machine learning project using tool such as mlflow, dvc etc',
+    author='Ankush',
+    license='',
+)
